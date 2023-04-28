8abe1d713460e7e934aa7c2969cdb1a7ba0ae6ad
zacccarie <53141547+zacccarie@users.noreply.github.com>
2023-04-28 09:46:20 +0200

    Create 3D force graph.js

---
+++ 3D force graph.js
@@ -0,0 +1,36 @@
+
+
+
+
+                    const Graph = ForceGraph3D()
+                        (document.getElementById('3d-graph'))
+                        
+                        .jsonUrl('../datasets/miserables.json')
+
+                        .height(window.innerHeight - 60)
+                        //.graphData(gData)
+                        .backgroundColor("black")
+                        //.nodeColor(node => "red")
+                        //.nodeOpacity(1)
+                        .linkColor(links => "white")
+                        .linkWidth(2)
+                        
+                        .showNavInfo(false)
+
+                        .nodeAutoColorBy('group')
+                        .nodeThreeObject(node => {
+                            const sprite = new SpriteText(node.id);
+                            sprite.material.depthWrite = false; // make sprite background transparent
+                            sprite.color = node.color;
+                            sprite.textHeight = 8;
+                            return sprite;
+                        });
+
+
+                    elementResizeDetectorMaker().listenTo(
+                        document.getElementById('3d-graph'),
+                        el => Graph.width(el.offsetWidth)
+                    );
+
+
+
