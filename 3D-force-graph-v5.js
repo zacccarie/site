37278fb1b0ef886abc7226337bc21e603f3f0a87
zacccarie <53141547+zacccarie@users.noreply.github.com>
2024-04-25 16:09:21 +0200

    Update 3D-force-graph-v5.js

--- 3D-force-graph-v5.js
+++ 3D-force-graph-v5.js
@@ -1,7 +1,7 @@
 const Graph = ForceGraph3D()
                         (document.getElementById('3d-graph'))
                         
-                        .jsonUrl('https://cdn.jsdelivr.net/gh/zacccarie/site@main/miserables.json')
+                        .jsonUrl('https://cdn.jsdelivr.net/gh/zacccarie/site@main/starwars-episode-5-interactions.json')
 
                         .height(window.innerHeight - 60)
                         //.graphData(gData)
